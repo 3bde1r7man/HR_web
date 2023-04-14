46fe590720d097bfe307a2c377c5204c623dcd75
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2023-04-14 18:09:39 +0200

    data.js

--- js/data.js
+++ js/data.js
@@ -1,33 +1,32 @@
 function JS() {
-    const firstname = document.getElementById("firstname").value;
-    const lastname = document.getElementById("lastname").value;
-    const email = document.getElementById("email").value;
-    const userid = document.getElementById("userid").value;
-    const address = document.getElementById("address").value;
-    localStorage.setItem(`firstname ${userid}`, firstname);
-    localStorage.setItem(`lastname ${userid}`, lastname);
-    localStorage.setItem(`email ${userid}`, email);
-    localStorage.setItem(`userid ${userid}`, userid);
-    localStorage.setItem(`address ${userid}`, address);
-    localStorage.setItem(`id`, userid);
-  }
-
-  function JS2() {
-    var total = 0;
-    const id = localStorage.getItem("id");
-    const gender = document.getElementById("gender").value;
-    const materialStatus = document.getElementById("materialStatus").value;
-    const phoneNum = document.getElementById("phoneNum").value;
-    const vacationNum = document.getElementById("vacationNum").value;
-    const ApprovedVactions = document.getElementById("ApprovedVactions").value;
-    const date = document.getElementById("date").value;
-    const salary = document.getElementById("salary").value;
-    localStorage.setItem(`gender ${id}`, gender);
-    localStorage.setItem(`materialStatus ${id}`, materialStatus);
-    localStorage.setItem(`phoneNum ${id}`, phoneNum);
-    localStorage.setItem(`vacationNum ${id}`, vacationNum);
-    localStorage.setItem(`ApprovedVactions ${id}`, ApprovedVactions);
-    localStorage.setItem(`date ${id}`, date);
-    localStorage.setItem(`salary ${id}`, salary);
-    localStorage.setItem(`Total employees`, ++total);
-  }
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const email = document.getElementById("email").value;
+  const userid = document.getElementById("userid").value;
+  const address = document.getElementById("address").value;
+  let data = `{ "firstName":"${firstname}" , "lastName":"${lastname}" , "email":"${email}","userid":"${userid}","address":"${address}" }`;
+  localStorage.setItem(userid, data);
+  localStorage.setItem(`id`, userid);
+}
+function JS2() {
+  const id = localStorage.getItem("id");
+  let data = localStorage.getItem(id);
+  const obj = JSON.parse(data);
+  const firstname = obj["firstName"];
+  const lastname = obj["lastName"];
+  const email = obj["email"];
+  const userid = obj["userid"];
+  const address = obj["address"];
+  const gender = document.getElementById("gender").value;
+  const materialStatus = document.getElementById("materialStatus").value;
+  const phoneNum = document.getElementById("phoneNum").value;
+  const vacationNum = document.getElementById("vacationNum").value;
+  const ApprovedVactions = document.getElementById("ApprovedVactions").value;
+  const date = document.getElementById("date").value;
+  const salary = document.getElementById("salary").value;
+  let totaldata = `{ "firstName":"${firstname}" , "lastName":"${lastname}" , "email":"${email}","userid":"${userid}","address":"${address}",
+                                          "gender":"${gender}","materialStatus":"${materialStatus}","phoneNum":"${phoneNum}","vacationNum":"${vacationNum}","ApprovedVactions":"${ApprovedVactions}"}`;
+  
+  localStorage.setItem(id,totaldata);
+  
+}
