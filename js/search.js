bc681f610d3c79f6a8189955ba29a7bfbc858dec
Seif <SE.SeifIbrahim@gmail.com>
2023-04-17 14:04:48 +0200

    Search functionality

--- js/search.js
+++ js/search.js
@@ -1,27 +1,41 @@
 const usersTmp = document.querySelector("[users-template]");
 const table = document.querySelector("[table]");
+const searchInput = document.querySelector("[searchInput]");
+let employees = [];
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value;
+    employees.forEach(employee => {
+        if(employee.name.toUpperCase().includes(value.toUpperCase())) {
+            employee.element.style.display = "";
+        } else {
+            employee.element.style.display = "none";
+        }
+        
+    });
+    
+})
+
 for(var i = 1; i < localStorage.length ; i++) {
     const userRow = usersTmp.content.cloneNode(true).children[0]
     const name = userRow.querySelector("[user-name]")
     const submit = userRow.querySelector("[submit-vac]")
     const edit = userRow.querySelector("[edit-user]")
-    var obj = JSON.parse(window.localStorage.getItem(i.toString()));
-    if(obj != null) {
-        var firstName = obj["firstName"];
-        var lastName = obj["lastName"];
+    var employee = JSON.parse(window.localStorage.getItem(i.toString()));
+    if(employee != null) {
+        
+        var firstName = employee.firstName;
+        var lastName = employee.lastName;
         name.textContent = firstName + " " + lastName;
-        submit.counter = i;
-        edit.counter = i;
+        submit.empID = i;
+        edit.empID = i;
         submit.onclick = function() {
-            localStorage.setItem("currentEmp", this.counter);
+            localStorage.setItem("currentEmp", this.empID);
         }
         edit.onclick = function() {
-            localStorage.setItem("currentEmp", this.counter);
+            localStorage.setItem("currentEmp", this.empID);
         }
         table.appendChild(userRow)
+        employees.push({name: name.textContent, element: userRow});
     }
-}
-
-function search() {
-    
 }
