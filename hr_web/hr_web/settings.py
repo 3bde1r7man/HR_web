f9feff813ca56e44f0c6ea6202e9046cf3498489
Ahmed Hanfy <ahanfybekheet@gmail.com>
2023-05-13 21:00:35 +0200

    make migrations

--- hr_web/hr_web/settings.py
+++ hr_web/hr_web/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hr_app.apps.MyappConfig',
+    'hr_app.apps.HrAppConfig',
 ]
 
 MIDDLEWARE = [
