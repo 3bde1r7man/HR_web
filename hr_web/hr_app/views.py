deda68d880c194fa4d95fb30427e8af37b1eb2cd
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2023-05-20 13:12:41 +0300

    added views & urls & config the settings

--- hr_web/hr_app/views.py
+++ hr_web/hr_app/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+
+
+
+
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+
+
+def index(request):
+    return render(request, 'projects.html')
+
+
+
+
+def add(request):
+    return render(request, 'add1.html')
+
+
+
+
+def add2(request):
+    return render(request, 'add2.html')
+
+
+
+
+def search(request):
+    return render(request,'search.html')
+
+
+
+
+def edit(request):
+    return render(request, 'edit.html')
+
+
+
+def vacation_form(request):
+    return render(request, 'vacation_form.html')
+
+
+def vacation_request(request):
+    return render(request, 'vacation_requests.html')
