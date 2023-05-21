a1db93bfbba6ec12811abc4b56ae2a65b1b0edce
you22ef <102329215+you22ef@users.noreply.github.com>
2023-05-21 17:02:01 +0200

    add_employee
    add_employee on 2 def

--- hr_web/hr_app/views.py
+++ hr_web/hr_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.views import View
 from .models import Employee
@@ -13,26 +13,25 @@ def index(request):
 class EmployeeView(View):
         
     def add_employee(self, request): 
+        if request.method == 'POST':
+            request.session['firstname'] = request.POST.get('firstname')
+            request.session['lastname'] = request.POST.get('lastname')
+            request.session['email'] = request.POST.get('email')
+            request.session['userid'] = request.POST.get('userid')
+            request.session['address'] = request.POST.get('address')
+            request.session.save()
+            return redirect('add_employee2')
         return render(request, 'add1.html')
 
     def add_employee2(self, request):
-        request.session['firstname'] = request.POST.get('firstname')
-        request.session['lastname'] = request.POST.get('lastname')
-        request.session['email'] = request.POST.get('email')
-        request.session['userid'] = request.POST.get('userid')
-        request.session['address'] = request.POST.get('address')
-        request.session.save()
-        return render(request, 'add2.html')
-    
-    def search(self, request):
-        material_status = request.POST.get('material_status')
-        gender = request.POST.get('gender')
-        vcation_days = request.POST.get('vcation_days')
-        approved_vacation = request.POST.get('approved_vacation')
-        phone = request.POST.get('phone')
-        salary = request.POST.get('salary')
-        date = request.POST.get('date')
-        if request.method == 'POST' and salary != None:
+        if request.method == 'POST':
+            material_status = request.POST.get('material_status')
+            gender = request.POST.get('gender')
+            vcation_days = request.POST.get('vcation_days')
+            approved_vacation = request.POST.get('approved_vacation')
+            phone = request.POST.get('phone')
+            salary = request.POST.get('salary')
+            date = request.POST.get('date')
             firstname = request.session['firstname']
             lastname = request.session['lastname']
             email = request.session['email']
@@ -40,6 +39,10 @@ class EmployeeView(View):
             address = request.session['address']
             data = Employee(firstname=firstname, lastname=lastname, email=email, userid=userid, address=address, material_status=material_status, gender=gender, phone=phone, vcation_days=vcation_days, approved_vacation=approved_vacation, salary=salary, date=date)
             data.save()
+            return redirect('search')
+        return render(request, 'add2.html')
+    
+    def search(self, request):
         return render(request, 'search.html')
     
     def dispatch(self, request, *args, **kwargs):
