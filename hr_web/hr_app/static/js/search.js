4e6e35fa16c7036be608e826e842a50dc98f8e17
Seif <SE.SeifIbrahim@gmail.com>
2023-05-26 01:37:13 +0300

    disabled submiting for 0 vacation days employees

--- hr_web/hr_app/static/js/search.js
+++ hr_web/hr_app/static/js/search.js
@@ -1,30 +1,23 @@
 $(document).ready(function() {
-    // Get the search input element
     var searchInput = document.getElementById('search-input');
-    var delayTimer; // Variable to hold the timer
-
-    // Add event listener for input changes
+    var delayTimer; 
     searchInput.addEventListener('input', function() {
-        clearTimeout(delayTimer); // Clear the previous timer
+        clearTimeout(delayTimer); 
 
-        // Set a new timer to delay the search
         delayTimer = setTimeout(function() {
-            // Get the search query from the input field
             var query = searchInput.value.trim();
-
-            // Perform AJAX request to the server
             $.ajax({
-                url: searchUrl, // Use the searchUrl variable here
+                url: searchUrl, 
                 method: 'GET',
                 data: { query: query },
                 success: function(response) {
-                    // Update the table body with the retrieved data
+                    
                     $('#employee-table-body').html($(response).find('#employee-table-body').html());
                 },
                 error: function(error) {
                     console.log(error);
                 }
             });
-        }, 500); // Delay for 500 milliseconds (adjust as needed)
+        }, 500); 
     });
 });
